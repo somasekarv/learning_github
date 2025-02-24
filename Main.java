215d9d6653b0f56119bbc05266907b4ade625839
Your Name <you@example.com>
2025-02-24 09:53:10 +0530

    commit ust

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package learning_github;
 public class Main {
     public static void main(String[] args){
         System.out.println("welcome to the world");
+        System.out.println("git learing");
     }
 }
     
