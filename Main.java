a24bcbb7349e17d633ccabf3dcc1ce2f5b5e4449
somasekar v <124439773+somasekarv@users.noreply.github.com>
2025-02-24 09:55:00 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ package learning_github;
 public class Main {
     public static void main(String[] args){
         System.out.println("welcome to the world");
-        System.out.println("git learing");
     }
 }
     
