c7e78375b7557f0f47f0392a5c5d1def060e3db5
Your Name <you@example.com>
2025-02-24 12:04:18 +0530

    removed

--- Main.java
+++ Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args){
         System.out.println("welcome to the world");
         System.out.println("git learing");
+<<<<<<< HEAD
         int var =200;
         
+=======
+>>>>>>> parent of 092786a (sfg)
     }
 }
     
